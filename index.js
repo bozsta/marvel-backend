212c257c68b510189e22ae061d1578faa71e2627
Fabien <fabien.l@skilters.com>
2021-02-04 23:46:59 +0100

    refacto limit & page

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const monggose = require('mongoose')
 require('dotenv').config()
 
 monggose.connect(process.env.MONGODB_URI, {
-    useCreateIndex: true,
-    useUnifiedTopology:true,
-    useNewUrlParser:true
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 })
 
 const PORT = process.env.PORT
@@ -20,11 +20,13 @@ app.use(cors())
 // Routes
 const comicsRoutes = require('./routes/comics')
 app.use('/comics', comicsRoutes)
+const characteressRoutes = require('./routes/characteres')
+app.use('/characteres', characteressRoutes)
 
 app.all('*', (req, res) => {
-    res.status(404).json({ message: 'URL not found' })
+  res.status(404).json({ message: 'URL not found' })
 })
 
-app.listen(PORT , () => {
-    `Serveur start on ${PORT}`
+app.listen(PORT, () => {
+  `Serveur start on ${PORT}`
 })
--- routes/characteres.js
+++ routes/characteres.js
@@ -2,12 +2,25 @@ const router = require('express').Router()
 const axios = require('axios')
 
 router.get('/all', async (req, res) => {
-    try {
-        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`)
-        res.status(200).json(response.data)
-    } catch (error) {
-        res.status(400).json({ error: { message: error.message } })
+  try {
+    let { limit, page } = req.query
+
+    if (isNaN(limit) || !Number.isInteger(Number(limit)) || limit < 0) {
+      throw new Error('Limit query params must be a positive integer')
     }
+    if (isNaN(page) || !Number.isInteger(Number(page)) || page < 0) {
+      throw new Error('page query params must be a positive integer')
+    }
+
+    limit = Number(limit)
+    const skip = limit * (page - 1)
+
+    const response = await axios.get(`${process.env.MARVEL_URI}/characters?apiKey=${process.env.MARVEL_API_KEY}&skip=${skip}&limit=${limit}`)
+    res.status(200).json(response.data)
+  } catch (error) {
+    // console.log('error', error.message)
+    res.status(400).json({ error: { message: error.message } })
+  }
 })
 
 module.exports = router
